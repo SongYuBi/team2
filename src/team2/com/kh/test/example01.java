9333871005f35096e32807354740c29635cf88f5
Song <Song@DESKTOP-AH7Q24J>
2020-06-04 14:22:07 +0900

    20200604 Song
    클래스를 만들었다.

---
+++ src/team2/com/kh/test/example01.java
@@ -0,0 +1,13 @@
+package team2.com.kh.test;
+
+public class example01 {
+
+	public void  example01_()
+	{
+		String name = " 너무 ";
+		for (int i = 0; i < 5; i++) {
+			System.out.println(name + "배가 고프다.");
+		}
+	}
+
+}
