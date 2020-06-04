ee8cf02a3ad69b155012ca1cef175a8b85334144
crowr <crowr@DESKTOP-LVM9E3S>
2020-06-04 14:21:59 +0900

    하라고 해서 했습니다.
    카메라 좀 껐으면 하는 마음을 담은 코드입니다.

---
+++ src/team2/com/kh/test/GoodbyeWorld.java
@@ -0,0 +1,10 @@
+package team2.com.kh.test;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("카메라 좀 껐으면");
+	}
+
+}
