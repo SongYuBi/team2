d953a7e76d8f09b7e632d2de9279f7bb5c9231aa
tnr <tnr@192.168.0.7>
2020-06-04 14:22:56 +0900

    test
    아무의미없음....

---
+++ src/team2/com/kh/test/Test.java
@@ -0,0 +1,16 @@
+package team2.com.kh.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("수정");
+			
+			System.out.println("완료....");
+			
+			System.out.println("1234567899874562130.");
+			
+			
+	}
+
+}
